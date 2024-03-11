277382f7f44ad6cc85f6d69705d8748d02e4136e
ElsaneHesenova <160469303+ElsaneHesenova@users.noreply.github.com>
2024-03-11 10:24:00 -0700

    Add files via upload

---
+++ ev tapsirigi/script.js
@@ -0,0 +1,372 @@
+  
+
+// // let age = +prompt("necenci sinfe gedilmelidir");
+// // if (age > 6 && age < 10) {
+// //     console.log("Siz ibtidai sinife gede bilersiniz")
+// // } else if (age >= 10 && age <= 14) {
+// //     console.log("Siz orta mektebe gedirsiniz");
+// // } else if (age >= 15 && age <= 17) {
+// //     console.log("Siz tam orta mektebe gedirsiniz");
+// // }
+ 
+// // else if (age >=17 && age < 21) {
+// //     console.log("Siz universitetde oxuyursunuz");
+// // }
+
+
+// // else if (age >= 21) {
+// //     console.log("is heyatindasiniz");
+// // }
+
+
+// // else{
+// //     console.log(" siz azyaslisiniz")
+// // }
+
+
+// //      TASK2
+// // let age = +prompt('yasinizi daxil edin');
+// // if( age>=18 && age<=70){
+// // console.log("ses vermede istirak ede bilersiniz")
+// // }
+
+// // else if(age>70){
+// // console.log(`ses vermeye ${age-70} il gecikmisiniz`);
+// // }
+
+// // else if(age<18){
+// //     console.log(`ses vermesi ucun ${age}il artiq zamani var`)
+
+// // }
+
+// // else{
+// //     console.log("istirakiniz qadagandir")
+// // }
+
+
+
+// // let num = +prompt("ededi daxil edin");
+// // if(num>0){
+// //     console.log("eded musbetdir");
+
+// // }
+
+// // else if(num<0){
+// //     console.log("eded menfidir");
+// // }
+
+
+
+
+
+// // const num_1= 10;
+// // const num_2= 7;
+// // const num_3= 8;
+
+// // function numbers(num1, num2, num3){
+
+// //     if(num1>num2 && num1<num3 || (num1<num2 && num1>num3)){
+
+// //         console.log( `${num1} ${num2} ve ${num3} Arasinda yerlesir`);
+// //     }
+// //     else if ((num2>num1 &&num2<num3)||(num2<num1 && num2>num3))
+// // {
+// //     console.log(`${num2} ${num1} ve ${num3} Arasinda yerlesir`);
+
+// // }
+// // else if ((num3>num2 && num3<num1)||(num3>num1 && num3<num2)){
+// //     console.log(`${num3} ${num2} ve ${num1} Arasinda yerlesir`);
+// // }
+
+// // }
+
+// // numbers( num_1, num_2, num_3,);
+
+
+
+
+
+//             //    TAPSIRIQ2
+
+
+
+
+
+
+            
+
+
+
+//             const athletes = [
+//                 {
+//                   name: "Usain Bolt",
+//                   sport: "Track and Field",
+//                   hasWorldRecord: true,
+//                   gender: "Male",
+//                   age: 35,
+//                   totalMedals: 8,
+//                   country: "Jamaica",
+//                 },
+//                 {
+//                   name: "Simone Biles",
+//                   sport: "Gymnastics",
+//                   hasWorldRecord: false,
+//                   gender: "Female",
+//                   age: 25,
+//                   totalMedals: 12,
+//                   country: "United States",
+//                 },
+//                 {
+//                   name: "Michael Phelps",
+//                   sport: "Swimming",
+//                   hasWorldRecord: true,
+//                   gender: "Male",
+//                   age: 37,
+//                   totalMedals: 28,
+//                   country: "United States",
+//                 },
+//                 {
+//                   name: "Katie Ledecky",
+//                   sport: "Swimming",
+//                   hasWorldRecord: true,
+//                   gender: "Female",
+//                   age: 25,
+//                   totalMedals: 10,
+//                   country: "United States",
+//                 },
+//                 {
+//                   name: "Allyson Felix",
+//                   sport: "Track and Field",
+//                   hasWorldRecord: false,
+//                   gender: "Female",
+//                   age: 36,
+//                   totalMedals: 9,
+//                   country: "United States",
+//                 },
+//                 {
+//                   name: "Nadia Comăneci",
+//                   sport: "Gymnastics",
+//                   hasWorldRecord: true,
+//                   gender: "Female",
+//                   age: 60,
+//                   totalMedals: 9,
+//                   country: "Romania",
+//                 },
+//                 {
+//                   name: "Birgit Fischer",
+//                   sport: "Canoeing",
+//                   hasWorldRecord: true,
+//                   gender: "Female",
+//                   age: 60,
+//                   totalMedals: 12,
+//                   country: "Germany",
+//                 },
+//                 {
+//                   name: "Mariana Pajón",
+//                   sport: "Cycling",
+//                   hasWorldRecord: false,
+//                   gender: "Female",
+//                   age: 30,
+//                   totalMedals: 4,
+//                   country: "Colombia",
+//                 },
+//                 {
+//                   name: "Wayne Gretzky",
+//                   sport: "Ice Hockey",
+//                   hasWorldRecord: false,
+//                   gender: "Male",
+//                   age: 61,
+//                   totalMedals: 4,
+//                   country: "Canada",
+//                 },
+//                 {
+//                   name: "Yuna Kim",
+//                   sport: "Figure Skating",
+//                   hasWorldRecord: false,
+//                   gender: "Female",
+//                   age: 31,
+//                   totalMedals: 6,
+//                   country: "South Korea",
+//                 },
+//               ];
+
+
+
+//             // 1. Dünya rekordu olan atletlərdən ibarət array yaradın.
+
+
+//             // function newArr(athletes){
+//             //     let recordmens = [];
+//             //     athletes.forEach((atlet) => {
+//             //         if(atlet.hasWorldRecord){
+//             //             recordmens.push(atlet);
+//             //         }
+//             //     });
+
+//             //     return recordmens
+//             // }
+
+//             // console.log(newArr(athletes));
+
+           
+            
+
+
+//             // 2. Medallarının sayı 10 və daha artıq olan qadın atletlərdən ibarət array yaradın.
+
+//         //     function newArr(athletes){
+//         //         let recordmens = [];
+//         //         athletes.forEach((atlet) => {
+//         //             if(atlet: totalMedals >=10 && atlet:gender=== "Female"){
+//         //                 recordmens.push(atlet);  
+//         //             }
+//         //         })
+//         //        return recordmens medal
+//         //     }
+
+//         //   console.log(newArr(athletes));
+
+// task 2
+// // const medal=(arr)=>{
+// //   const filtered=arr.filter(
+// //     (item)=>item.totalMedals >=10 && item.gender=== "Female"
+// //   );
+// //   return  filtered
+// // }
+
+// // console.log(medal(athletes));
+        
+
+//             // 3. Atletlərin adlarından ibarət array yaradın.
+
+
+// //             task3
+
+// // const atlet=(arr)=>{
+// //  const filtered= arr.map(
+// //   (item)=>item.name);
+ 
+// //   return filtered
+// // }
+           
+// // console.log(atlet(athletes));
+
+
+// // task4
+
+// // const athleteInfo=athletes.map((athlete)=>({
+// //   name:athlete.name,
+// //   country:athlete.country,
+// // }));
+
+
+
+
+          
+              
+
+
+
+
+
+
+//             // 5. Bütün atletlərin ümumi medallarının sayını hesablayın.
+
+
+
+
+
+const books = [
+  {
+    title: "JavaScript: The Good Parts",
+    author: "Douglas Crockford",
+    genre: "Programming",
+    pageCount: 176,
+    isRead: true,
+  },
+  {
+    title: "To Kill a Mockingbird",
+    author: "Harper Lee",
+    genre: "Fiction",
+    pageCount: 281,
+    isRead: false,
+  },
+  {
+    title: "The Great Gatsby",
+    author: "F. Scott Fitzgerald",
+    genre: "Fiction",
+    pageCount: 180,
+    isRead: true,
+  },
+  {
+    title: "1984",
+    author: "George Orwell",
+    genre: "Dystopian",
+    pageCount: 328,
+    isRead: false,
+  },
+  {
+    title: "The Hobbit",
+    author: "J.R.R. Tolkien",
+    genre: "Fantasy",
+    pageCount: 310,
+    isRead: true,
+  },
+];
+
+            
+
+
+// 1. Fiction tipindeki kitablari consola yazdirin(filter)
+
+// const book=(arr)=>{
+//    const filtered=arr.filter((item)=>item.genre==="Fiction");
+//    return filtered;
+// };
+// console.log(book(books));
+
+
+// 2. Toplam seyfe sayi 200'den chox olan kitapların sayini tapin.
+
+// const book=(arr)=>{
+//   const filtered=arr.filter((item)=>item.pageCount>=200 &&item.books);
+
+//   return filtered;
+
+// }
+// console.log(book(books));
+
+
+
+// verilen bir kitabın oxunma veziyyetini yenileyin.
+
+// const isRead=(tittle)=>{
+//   const findBook=books.find((item)=>item.title ===title);
+//   findBook.isRead=
+// }
+
+
+
+
+
+
+
+
+
+
+// task2
+// Fiction olan kitablarin ad ve authordan ibaret teze objectden ibaret arr yaradin
+
+
+let fictionList = books.filter(book=>{
+  if(book.genre === 'Fiction'){
+    return{name:book.title,author:book.author}
+  }
+})
+
+console.log(fictionList);
+
+
+
+        
+
---
+++ javascript/script.js
@@ -0,0 +1,42 @@
+let number=+prompt("bir reqem daxil edin")
+
+if( number %2===0){
+console.log ("eded cutdur");
+
+}
+
+else{
+    console.log("eded tekdir")
+}
+
+
+
+
+let age=+prompt("yasinizi daxil edin");
+let money=+prompt("pulunuzu daxil edin");
+if( age>=18 & money>=1000){
+console.log("daxil etdiyin eded serti odeyir");
+}
+
+else{ 
+    console.log("daxil etdiyin eded serti odeyir");
+}
+
+
+
+let exam1= +prompt("exam1");
+let exam2= +prompt("exam2");
+let exam3= +prompt("exam3");
+let avarage= exam1*0.3 + exam2*0.3 + final *0.4;
+
+if( avarage = exam1 >=51 && avarage<70){
+
+}
+
+else{
+
+}
+
+
+
+
